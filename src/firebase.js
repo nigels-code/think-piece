0aed4891212a4cb99084547bf1213981f96de9d2
Nigel <75135800+nigels-code@users.noreply.github.com>
2021-07-26 09:39:21 +0100

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
   projectId: process.env.REACT_APP_PROJECT_ID,
   storgaeBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGEING_SENDER_ID,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID
 };
 firebase.initializeApp(firebaseConfig);
